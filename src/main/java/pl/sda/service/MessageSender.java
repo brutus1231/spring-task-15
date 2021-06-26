b89ede244690964f2489184cd76de55462a46376
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-06-26 15:57:47 +0200

    spring context

--- src/main/java/pl/sda/service/MessageSender.java
+++ src/main/java/pl/sda/service/MessageSender.java
@@ -1,15 +1,20 @@
 package pl.sda.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
 @Component
-@RequiredArgsConstructor
 public class MessageSender {
 
     private final DayCalculator calculator;
+
+    public MessageSender(@Qualifier("workDayCalculator")
+                                 DayCalculator calculator) {
+        this.calculator = calculator;
+    }
 
     public void send(String message, LocalDate date) {
         System.out.println(calculator.getClass().getName());
